cae5211f82f454786faf0e15a58f12819c378ef2
hanifaslamm <aslamhanif141@gmail.com>
2023-11-17 15:15:55 +0700

    ..

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mygeometry',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={},
+    scripts=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
